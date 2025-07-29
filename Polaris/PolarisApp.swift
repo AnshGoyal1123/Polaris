0a354bae1cdb8375ce7f31badca982ecb540d34a
Ansh Goyal <agoyal2311@gmail.com>
2025-07-28 21:49:29 -0400

    Initial Commit

---
+++ Polaris/PolarisApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolarisApp.swift
+//  Polaris
+//
+//  Created by Ansh Goyal on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PolarisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PolarisTests/PolarisTests.swift
@@ -0,0 +1,17 @@
+//
+//  PolarisTests.swift
+//  PolarisTests
+//
+//  Created by Ansh Goyal on 7/28/25.
+//
+
+import Testing
+@testable import Polaris
+
+struct PolarisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
